65355e5b09386579293150190ecfc4dd77f639b2
khaled39ab <khaled39ab@gmail.com>
2022-06-01 22:40:00 +0600

    learn google sign out authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 function App() {
   const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth, provider)
     .then( result => {
       const user = result.user;
+      setUser(user);
       console.log(user);
+    })
+    .catch(err => console.log(err))
+  }
+
+  const handleSignOut = () =>{
+    signOut(auth)
+    .then(() =>{
+      setUser({})
     })
     .catch(err => console.log(err))
   }
@@ -17,6 +29,10 @@ function App() {
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button onClick={handleSignOut}>Sign Out</button>
+      <h2>Name: {user.displayName}</h2>
+      <h3>Email: {user.email}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
